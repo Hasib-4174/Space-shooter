c39230aa162882dab9286ef3514c2a1c93d74f50
Hasib-4174 <hasibkhan4174@gmail.com>
2025-09-06 03:30:30 +0600

    add player & stars

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+from random import randint
 
 # setup pygame
 
@@ -7,6 +8,18 @@ pygame.init()
 WINDOW_WIDTH, WINDOW_HEIGHT = 1280, 720
 display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 pygame.display.set_caption("Space Shooter")
+
+# plain surface
+surf = pygame.Surface((50, 50))
+surf.fill('yellow')
+x = 0
+
+# image surface
+player_surf = pygame.image.load('./images/player.png').convert_alpha()
+star = pygame.image.load('./images/star.png').convert_alpha()
+star_pos = [(randint(0, WINDOW_WIDTH), randint(0, WINDOW_HEIGHT)) for _ in range(20)]
+
+
 
 is_running = True
 
@@ -17,7 +30,13 @@ while is_running:
             is_running = False
 
     # draw game
-    display_surface.fill('yellow')
+    display_surface.fill('darkgray')
+    x+=.1
+    #display_surface.blit(surf, (x, 200))
+    display_surface.blit(player_surf, (x, 300))
+    for pos in star_pos:
+        display_surface.blit(star, pos)
+    
 
 
     pygame.display.update()
